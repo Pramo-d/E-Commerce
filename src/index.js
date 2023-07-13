63c3d9fbb897c20d90c7a1f7558260caa232c901
Pramo-d <promody1444@gmail.com>
2023-07-13 19:19:34 +0530

    ecommerce

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//    <div>
-
-//     <App />
-//     </div>
-
-// );
 import ReactDOM from "react-dom";
 import App from "./App.js";
 
