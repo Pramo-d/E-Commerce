477174c2b0f5e5aeda8162b251ec4a98d5cd017f
Pramo-d <promody1444@gmail.com>
2023-07-18 01:14:05 +0530

    adding some feature

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -4,12 +4,13 @@ const Product = () => {
   const [productID, setProductID] = useState("");
   const [sellingPrice, setSellingPrice] = useState("");
   const [productName, setProductName] = useState("");
+  const [productCategory, setProductCategory] = useState("");
   const [DataList, setDataList] = useState([]);
 
   const submitHandler = (event) => {
     event.preventDefault();
 
-    const Data = { productID, sellingPrice, productName };
+    const Data = { productID, sellingPrice, productName, productCategory };
     console.log(Data);
     setDataList((prevList) => [...prevList, Data]);
 
@@ -26,6 +27,9 @@ const Product = () => {
   const productNameHandler = (event) => {
     setProductName(event.target.value);
   };
+  const productCategoryHandler = (event) => {
+    setProductCategory(event.target.value);
+  };
 
   const handleDelete = (index) => {
     setDataList((prevList) => {
@@ -34,12 +38,12 @@ const Product = () => {
       return updatedList;
     });
   };
-  var val = 0;
-  const amount = DataList.map((item) => {
-    val += parseInt(item.sellingPrice);
-    console.log(val);
-    return val;
-  });
+  // var val = 0;
+  // const amount = DataList.map((item) => {
+  //   val += parseInt(item.sellingPrice);
+  //   console.log(val);
+  //   return val;
+  // });
 
   return (
     <div>
@@ -61,24 +65,70 @@ const Product = () => {
             value={productName}
             onChange={productNameHandler}
           />
+
+          <label>Choose a Category</label>
+          <select   onChange={productCategoryHandler}>
+            <option id="Electronics" value={productCategory} >Electronics </option>
+            <option id="Food"  >Food </option>
+            <option  id="SkinCare">SkinCare</option>
+          </select>
           <button type="submit">Add Product</button>
         </div>
       </form>
 
       <h3>Products:</h3>
-      <ul>
+      
+      <h4 id="Electronics">
+        Electronics Items
+       
+        <ul>
         {DataList.map((Data, index) => (
           <li key={index}>
             <p>
               {" "}
-              {Data.sellingPrice} {Data.productName}
+              {Data.sellingPrice} {Data.productCategory} {Data.productName}
             </p>
             <button onClick={handleDelete}>Delete Product</button>
           </li>
         ))}
       </ul>
-      <h4>Total Value Worth of Products:{amount}</h4>
+
+      </h4>
+
+      <h4 id="Food">
+        Food Items
+        <ul>
+        {DataList.map((Data, index) => (
+          <li key={index}>
+            <p>
+              {" "}
+              {Data.sellingPrice} {Data.productCategory} {Data.productName}
+            </p>
+            <button onClick={handleDelete}>Delete Product</button>
+          </li>
+        ))}
+      </ul>
+        </h4>
+      <h4 id="SkinCare">
+        SkinCare Items
+        <ul>
+        {DataList.map((Data, index) => (
+          <li key={index}>
+            <p>
+              {" "}
+              {Data.sellingPrice} {Data.productCategory} {Data.productName}
+            </p>
+            <button onClick={handleDelete}>Delete Product</button>
+          </li>
+        ))}
+      </ul>
+        </h4>
+     
+      {/* <h4>Total Value Worth of Products:{amount}</h4> */}
     </div>
   );
 };
+
 export default Product;
+
+ 
